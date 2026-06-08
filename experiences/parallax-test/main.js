66eb5b54de22289e975d245bb6f5012e007490f3
mirouwhite <support.traca@gmail.com>
2026-06-08 20:02:02 +0100

    fix: Use CDN imports for Three.js in parallax test

--- experiences/parallax-test/main.js
+++ experiences/parallax-test/main.js
@@ -1,5 +1,5 @@
-import * as THREE from 'three';
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+import * as THREE from 'https://unpkg.com/three@0.160.0/build/three.module.js';
+import { OrbitControls } from 'https://unpkg.com/three@0.160.0/examples/jsm/controls/OrbitControls.js';
 
 class ParallaxTest {
     constructor() {
